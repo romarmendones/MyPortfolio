98fc11d4d5ff15a9c54f38e4e8248d1d01e717dd
Romar Mendones <romarmendones24@gmail.com>
2025-11-22 21:03:08 +0800

    update

--- src/components/About.js
+++ src/components/About.js
@@ -460,7 +460,7 @@ const About = () => {
               >
                 <motion.a
                   href="https://github.com"
-                  target="https://github.com/romarmendones?tab=repositories"
+                  target="https://github.com/romarmendones"
                   rel="noopener noreferrer"
                   whileHover={{ scale: 1.1, y: -2, rotate: 5 }}
                   whileTap={{ scale: 0.95 }}
